0ec77f6c3fc33426098597c4b07460e51637e7f5
Tadeusz Szar <tadeusz.szar@gmail.com>
2023-03-16 16:55:21 +0100

    wip

---
+++ build.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++17 -o csr_acc csr_acc.cpp && ./csr_acc
--- csr_acc.cpp
+++ csr_acc.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 
 enum class service_restriction
 {
@@ -16,9 +17,9 @@ enum class analysis_type
   fatigue
 };
 
-const double _L_rule = 175.15; //m
+const double _L_RULE = 175.15; //m
 const double _B = 32.2;        //m
-const double _T_sc = 13.3;     //m
+const double _T_SC = 13.3;     //m
 const double _T_LC = 7.09;     //m
 const double _D = 19.1;        //m
 const double _V = 14.45;       //knots
@@ -32,10 +33,28 @@ const bool _BilgeKeel = true;
 const double _grav = 9.81; //mm/s2
 const double _PI = 3.14159265358979323846;
 
+//Acceleration parameter
+double calc_a0(double L, double CB){
+  return (1.58-0.47*CB)*((2.4/sqrt(L))+(34/L)-(600/(L*L)));
+};
+
+//Vertical coorditnate of the ship rotation centre, in [m]
+double calc_rot_centre(double D, double T_LC){
+  return std::fmin((D/4)+(T_LC/2),D/2);
+};
+
+//Ratio between draught at a loading condtion and scantling draught, not less then 0.5
+double calc_fT(double T_LC, double T_SC){
+  return std::fmin(T_LC/T_SC,0.5);
+};
+
 
 
 int main()
 {
-  std::cout << "Hello\n"; 
+  std::cout << "Hello\n";
+  std::cout << "a0 = " << calc_a0(_L_RULE, _CB) << "\n";
+  std::cout << "R = " << calc_rot_centre(_D, _T_LC) << "\n";
+  std::cout << "fT = " << calc_fT(_T_LC, _T_SC) << "\n";
   return 0;
 }
